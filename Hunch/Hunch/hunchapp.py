117e8d29b95f6971dd28985d4389bf663578feb0
agconti <agc11d@gmail.com>
2013-07-24 18:26:44 -0400

    made heroku ready

--- Hunch/Hunch/hunchapp.py
+++ Hunch/Hunch/hunchapp.py
@@ -25,12 +25,12 @@ def search_querry():
                             'show_entries.html', 
                             queried_resturants=queried_resturants, 
                             weather=weather, 
-                            walking_day=False
+                            walking_day=weather_bool
                             )
 
 
 if __name__ == '__main__':
-        #import os  
-        #port = int(os.environ.get('PORT', 33507)) 
-        #app.run(host='0.0.0.0', port=port)
-        app.run(debug=True)
+        import os  
+        port = int(os.environ.get('PORT', 33507)) 
+        app.run(host='0.0.0.0', port=port)
+        #app.run(debug=True)
