5cf785fe2e305192a45aac230781870f3cf7118b
agconti <agc11d@gmail.com>
2013-07-29 16:01:40 -0400

    updated radio form

--- Hunch/Hunch/hunchapp.py
+++ Hunch/Hunch/hunchapp.py
@@ -82,7 +82,7 @@ def random_search_querry():
 @app.route('/results/more_<past_val>_hunches<ind>', methods=['POST'])
 def more_results(past_val, ind):
     
-    if request.form["action"] == "Get the next Hunch!":
+    if request.form["next_hunch"] == "Get the next Hunch!":
         
         # increment the index reference
         ind = int(ind)
@@ -116,14 +116,13 @@ def more_results(past_val, ind):
 def re_render(weather_bool, past_val, ind):
     '''
     re-renders the show entries template.
-    for use with bad weather radio switch
+    for use with bad weather button
     '''
     if request.form["weather_button"] == "Bad Weather?":
         
-        # increment the index reference
+        # check index is an int
         ind = int(ind)
-        ind += 1 
-        
+
         # gets the the previous list of queried restaurants
         # by using the last search term (past_val)
         queried_restaurants = hf.find_lunch(past_val)
@@ -133,11 +132,7 @@ def re_render(weather_bool, past_val, ind):
         queried_restaurants = queried_restaurants[ind]
 
         weather = hf.get_weather()
-
-        # for efficiency, I've limited the number of queried restaurants to 4
-        # this handles when we go beyond 4 results by returning us to the home screen.
-        if ind > 3:
-            return render_template('home.html', last_result=True)
+        
         # renders template, and passes index and past search term for later use
     
         return render_template(
